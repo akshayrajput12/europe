d379e6d73a053b27493de8531ca2b1c8b5dc2e83
akshayrajput12 <akshayrajput2616@gmail.com>
2025-08-31 12:12:03 +0530

    stand

--- src/custom-stands/components/StandProjectTextSection.tsx
+++ src/custom-stands/components/StandProjectTextSection.tsx
@@ -1,34 +1,37 @@
-import { customStandsData } from "@/data/custom-stands"
+import type { StandProjectText } from "@/data/custom-stands"
 
-export default function ProjectsSection() {
-  const { StandProjectText } = customStandsData
+interface StandProjectTextSectionProps {
+  standProjectTextData: StandProjectText
+}
 
+export default function ProjectsSection({ standProjectTextData }: StandProjectTextSectionProps) {
   return (
-    <section className="py-12 md:py-16 bg-secondary  text-white">
+    <section className="py-12 md:py-16 bg-secondary text-white">
       <div className="container mx-auto px-4">
         <div className="text-center mb-8 md:mb-12">
           {/* Title (optional) */}
-          {StandProjectText.title && (
+          {standProjectTextData.title && (
             <h2 className="text-2xl md:text-4xl font-bold mb-6">
-              {StandProjectText.title}{" "}
-              {StandProjectText.highlight && (
-                <span className="text-[#A5CD39]">{StandProjectText.highlight}</span>
+              {standProjectTextData.title}{" "}
+              {standProjectTextData.highlight && (
+                <span className="text-[#A5CD39]">{standProjectTextData.highlight}</span>
               )}
             </h2>
           )}
 
           {/* Subtitle (optional) */}
-          {/* {StandProjectText.highlight && (
+          {/* {standProjectTextData.highlight && (
             <p className="text-lg md:text-xl font-medium mb-4 text-gray-200">
-              {StandProjectText.highlight}
+              {standProjectTextData.highlight}
             </p>
           )} */}
 
           {/* Description */}
-          {StandProjectText.description && (
-            <p className="text-gray-300 max-w-4xl mx-auto leading-relaxed text-base md:text-lg">
-              {StandProjectText.description}
-            </p>
+          {standProjectTextData.description && (
+            <div 
+              className="text-white max-w-4xl mx-auto leading-relaxed text-base md:text-lg"
+              dangerouslySetInnerHTML={{ __html: standProjectTextData.description }}
+            />
           )}
         </div>
 
