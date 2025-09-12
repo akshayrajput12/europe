134a16bfe0887e36b80187fe93a710cba93224ef
akshayrajput12 <akshayrajput2616@gmail.com>
2025-09-12 15:33:54 +0530

    ui

--- src/app/major-exhibiting-country/page.tsx
+++ src/app/major-exhibiting-country/page.tsx
@@ -1,5 +1,8 @@
 import CountryPage from "@/countries/page"
 
+// ISR Configuration - Revalidate every 30 days (same as stand pages)
+export const revalidate = 2592000; // 30 days in seconds
+
 export default function Page() {
   return <CountryPage />
 }
